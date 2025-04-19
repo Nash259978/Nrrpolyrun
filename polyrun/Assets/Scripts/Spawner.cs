97340ad3f4536e0c4055e7eb80af77c0a6eb3917
nash <breadsnugget@gmail.com>
2025-04-19 13:40:09 -0400

    step 40ish

---
+++ polyrun/Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("ChallengeObj Game Object")]
+    public GameObject challengeObject;
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 1f;
+    [Header("Default Spawn Time")]
+    public float spawnTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("InstantiateObjects", spawnDelay, spawnTime);
+    }
+
+    void InstantiateObject()
+    {
+        Instantiate(challengeObject, transform.position, transform.rotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
